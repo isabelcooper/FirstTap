cfeae2cd5c9adf36a2948fd1f7997e3db724c531
isabelcooper <isabel@triptease.com>
2020-01-20 11:46:42 +0000

    accept locally defined port for server

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,17 @@ import {routes, Routing} from "http4js/core/Routing";
 import {Method} from "http4js/core/Methods";
 import {NativeHttpServer} from "http4js/servers/NativeHttpServer";
 import {ResOf} from "http4js/core/Res";
+require('dotenv').config();
 
 export class Server {
   private server: Routing;
 
   constructor(private port: number = 3330) {
-    this.server = routes(Method.GET, '/health', async() => ResOf(200))
-      .asServer(new NativeHttpServer(this.port));
+    this.server = routes(Method.GET, '/health', async() => {
+      console.log('in health endpoint');
+      return ResOf(200)
+    })
+      .asServer(new NativeHttpServer(parseInt(process.env.PORT!) || this.port));
   }
 
   start() {
