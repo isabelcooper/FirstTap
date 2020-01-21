7fbc9073ec88695fbf5e4599ec211da4e6a8b2c3
isabelcooper <isabel@triptease.com>
2020-01-21 13:46:25 +0000

    auth logging to debug

--- utils/Authenticator.ts
+++ utils/Authenticator.ts
@@ -18,8 +18,13 @@ export class InternalAuthenticator implements Authenticator {
     return asHandler(async (req: Req) => {
       const header = req.header('authorization');
       const authHeader = (header && header.slice(6)) || '';
-      const [username, password] = Buffer.from(authHeader, 'base64').toString().split(':');
-
+      console.log(authHeader);
+      let buffer = Buffer.from(authHeader, 'base64');
+      console.log('buffer', buffer);
+      let fullString = buffer.toString();
+      console.log('fullString', fullString);
+      const [username, password] = fullString.split(':');
+      console.log(username, password);
       if (username !== this.credentials.username || password !== this.credentials.password) {
         return ResOf(401, 'Client not authenticated', {'WWW-Authenticate': 'Basic realm="Entity resource"'});
       }
