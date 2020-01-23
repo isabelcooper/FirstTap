59955b7ff052e5446bb1e1c93462cab7acf657e6
isabelcooper <isabel@triptease.com>
2020-01-23 10:04:28 +0000

    extract fileHandler - testing still covered in server

---
+++ utils/FileHandler.ts
@@ -0,0 +1,17 @@
+import {Handler, ResOf} from "http4js";
+import {TokenManagerClass} from "../src/userAuthtoken/TokenManager";
+import {TransactionType} from "../src/signup-logIn-logout/EmployeeStore";
+import {Req} from "http4js/core/Req";
+import {Res} from "http4js/core/Res";
+import {TransactionManagerClass} from "../src/transactions/TransactionManager";
+import * as fs from "fs";
+
+export class FileHandler implements Handler {
+
+  public async handle(req: Req): Promise<Res> {
+    const fileName = req.pathParams.fileName;
+    const fileType = req.uri.path().split('.')[1] === 'css' ? 'css' : 'html';
+    return ResOf(200, (fs.readFileSync(`./docs/privacy/${fileName}.${fileType}`)).toString())
+  }
+}
+// TODO move tests over from server
